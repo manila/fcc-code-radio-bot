2624516b234d977a4350686847cf8ab72598be89
manila <manila@users.noreply.github.com>
2020-09-26 23:51:44 -0500

    Added play and stop controls

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-import Discord from "discord.js";
+import { Client, Message } from "discord.js";
 
-const token = process.env!.TOKEN;
-const client = new Discord.Client();
+const stream_url = "https://coderadio-admin.freecodecamp.org/radio/8010/low.mp3";
+const token = process.env.TOKEN;
+const client = new Client();
 
 client.on("ready", () => {
-client.on('message', async message => {
-		if (message!.member!.voice!.channel) {
-					const connection = await message!.member!.voice!.channel!.join();
-					connection.play("https://coderadio-admin.freecodecamp.org/radio/8010/low.mp3")
+	let dispatcher: any = null;
+	let connection: any = null;
+
+	client.on("message", async (message: Message) => {
+		if (
+			message.content === "!play" &&
+			message.member!.voice.channel
+		) {
+			connection = await message.member!.voice.channel.join();
+			dispatcher = connection.play(stream_url);
+		} else if (message.content === "!stop" && dispatcher) {
+			dispatcher.destroy()	
+			connection.disconnect()
 		}
 	});
-})
+});
 
-client.login(token)
+client.login(token);
